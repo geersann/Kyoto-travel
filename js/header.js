53971d023576e4759763a5da87d651d4b274f6a1
geersann <148795588+geersann@users.noreply.github.com>
2024-06-04 23:54:43 +0000

    update burger, add inst + tiktock

--- js/header.js
+++ js/header.js
@@ -1,4 +1,5 @@
 document.querySelector('.burger').addEventListener('click', function() {
     this.classList.toggle('active');
     document.querySelector('.header__nav').classList.toggle('open');
+    document.querySelector('body').classList.toggle('hiden');
 })
